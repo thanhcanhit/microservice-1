44bf4fc013eea832efa3335f4e176ed34d6f60d2
thanhcanhit <thanhcanh.dev@gmail.com>
2025-04-08 15:53:52 +0700

    Use .env file and optimize the Docker file

--- product-service/index.js
+++ product-service/index.js
@@ -5,6 +5,11 @@ const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const productRoutes = require('./routes/productRoutes');
 
+// For rate limiting
+const rateLimit = require('express-rate-limit');
+// For request timeout
+const timeout = require('connect-timeout');
+
 // Load environment variables
 dotenv.config();
 
@@ -17,6 +22,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Set request timeout
+const apiTimeout = process.env.API_TIMEOUT || 30000;
+app.use(timeout(apiTimeout));
+app.use((req, res, next) => {
+  if (!req.timedout) next();
+});
+
+// Apply rate limiting
+const limiter = rateLimit({
+  windowMs: process.env.RATE_LIMIT_WINDOW_MS || 15 * 60 * 1000, // 15 minutes by default
+  max: process.env.RATE_LIMIT_MAX || 100, // Limit each IP to 100 requests per windowMs
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use(limiter);
 
 // Routes
 app.use('/api/products', productRoutes);
